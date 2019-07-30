0df92174385a82381cb3b97376ae7e935384918a
Sarin Na Wangkanai <sarin@wangkanai.com>
2019-07-30 12:02:58 +0700

    add repo open graph

---
+++ src/Webmaster.TagHelpers/src/CanonicalTagHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wangkanai.Webmaster.TagHelpers
+{
+    public class CanonicalTagHelper : TagHelper
+    {
+    }
+
+    public class AlternateTagHelper : TagHelper
+    {
+
+    }
+}
