7f866c586f07f3815633020faa9660ae602ecdb6
Sarin Na Wangkanai <sarin@wangkanai.com>
2019-08-20 23:09:18 +0700

    refactor language route contraint test

--- src/Webmaster.Routing/src/Language/ThaiLangaugeRouteContraint.cs
+++ src/Webmaster.Routing/src/Language/ThaiLangaugeRouteContraint.cs
@@ -2,11 +2,17 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Wangkanai.Webmaster.Routing.Language
 {
     public class ThaiLanguageRouteContraint : RegexRouteConstraint
     {
         public ThaiLanguageRouteContraint() : base("[\u0E00-\u0E7F]") { }
+    }
+
+    public class LanguageRouteContraint : RegexRouteConstraint
+    {
+        public LanguageRouteContraint() : base("[a-z]") { }
     }
 }
---
+++ src/Webmaster.Routing/test/Language/ThaiLanguageRouteContraintTest.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Routing.Constraints;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Wangkanai.Webmaster.Routing.Language;
+using Xunit;
+
+namespace Wangkanai.Webmaster.Routing.Tests.Language
+{
+    public class ThaiLanguageRouteContraintTest : LanguageRouteContraintBaseTest
+    {
+        [Theory]
+        [InlineData("กรุงเทพ", true)]
+        [InlineData("Bangkok", false)]
+        [InlineData("กรุงเทพ1", true)]      // want it to be false
+        [InlineData("กรุงเทพabc", true)]    // want it to be false
+        [InlineData("Bangkokกทม", true)]  // want it to be false 
+        public void ThaiRegexContraintFromInput(string routeValue, bool shouldMatch)
+        {
+            // arrage            
+            var contraint = new ThaiLanguageRouteContraint();
+            var values = new RouteValueDictionary(new { controller = routeValue });
+
+            // act
+            bool match = Match(contraint, values);
+
+            // assert
+            Assert.Equal(shouldMatch, match);
+        }
+
+        private bool Match(ThaiLanguageRouteContraint contraint, RouteValueDictionary values)
+        {
+            return contraint.Match(
+                new DefaultHttpContext(),
+                route: new Mock<IRouter>().Object,
+                routeKey: "controller",
+                values: values,
+                routeDirection: RouteDirection.IncomingRequest);
+        }
+    }
+}
