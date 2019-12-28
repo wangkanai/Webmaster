711631fa9aa6766d99ceacdf430b90f590b48537
Sarin Na Wangkanai <sarin@wangkanai.com>
2019-12-28 20:13:08 +0700

    nuget include reference package

--- run.ps1
+++ run.ps1
@@ -1,6 +1,6 @@
-./dotnet-install.ps1 -Channel Current -version 3.0.100
+#./dotnet-install.ps1 -Channel Current -version 3.0.100
 dotnet --version
 dotnet restore
-dotnet build
+dotnet build -Channel Release
 dotnet test
-dotnet pack ./src/Webmaster/src/Wangkanai.Webmaster.csproj -c Release -o .\artifacts
+dotnet pack ./src/Webmaster/src/Wangkanai.Webmaster.csproj -Channel Release -o .\artifacts
