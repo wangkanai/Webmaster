a4d7ba84455c736266e40755705ae3dad5011e85
Sarin Na Wangkanai <sarin@wangkanai.com>
2020-02-05 20:58:15 +0700

    Refactor test project

--- sample/Startup.cs
+++ sample/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Wangkanai.Webmaster.Core.Builders;
 
 namespace WebApp
 {
@@ -23,6 +24,8 @@ namespace WebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddWebmaster();
+            
             services.AddControllersWithViews();
         }
 
@@ -46,6 +49,8 @@ namespace WebApp
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseWebmaster();
 
             app.UseEndpoints(endpoints =>
             {
