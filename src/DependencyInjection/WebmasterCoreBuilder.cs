9b9318ebabe81b959f9c9499ea8d7af105d3bfba
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2020-02-05 18:04:52 +0700

    Application Dependency injection

---
+++ src/DependencyInjection/WebmasterCoreBuilder.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) 2014-2020 Sarin Na Wangkanai, All Rights Reserved.
+// The Apache v2. See License.txt in the project root for license information.
+
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Wangkanai.Webmaster.Builders
+{
+    public class WebmasterCoreBuilder : IWebmasterCoreBuilder
+    {
+        public WebmasterCoreBuilder(IServiceCollection services)
+        {
+            if (services == null)
+                throw new ArgumentNullException(nameof(services));
+
+            Services = services;
+        }
+
+        public IServiceCollection Services { get; private set; }
+    }
+}
