9bca059ce29dd383a78707704ec7f9445ddfeb90
Sarin Na Wangkanai <sarin@wangkanai.com>
2020-02-21 21:19:14 +0700

    Version can't specify how many field count

--- src/TagHelpers/VersionTagHelper.cs
+++ src/TagHelpers/VersionTagHelper.cs
@@ -4,21 +4,25 @@ using Microsoft.AspNetCore.Razor.TagHelpers;
 
 namespace Wangkanai.Webmaster.TagHelpers
 {
-    [HtmlTargetElement(ElementName, TagStructure = TagStructure.WithoutEndTag)]
+    [HtmlTargetElement(ElementName, Attributes = FieldCountAttributeName, TagStructure = TagStructure.WithoutEndTag)]
     public class VersionTagHelper : TagHelper
     {
         private const string ElementName = "version";
+        private const string FieldCountAttributeName = "field";
+
+        [HtmlAttributeName(FieldCountAttributeName)]
+        public int FieldCount { get; set; } = 3;
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-            if (context == null)
+            if (context is null)
                 throw new ArgumentNullException(nameof(context));
-            if (output == null)
+            if (output is null)
                 throw new ArgumentNullException(nameof(output));
 
             output.TagName = "span";
             output.TagMode = TagMode.StartTagAndEndTag;
-            var version = Assembly.GetEntryAssembly()?.GetName().Version.ToString();
+            var version = Assembly.GetEntryAssembly()?.GetName().Version.ToString(FieldCount);
             output.Content.Append(version);
         }
     }
